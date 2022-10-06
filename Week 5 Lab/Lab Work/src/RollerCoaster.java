c777f15a78b3c793f5473a903d92feed7ad9f82d
Khaled Galal <th.be03@gmail.com>
2022-10-06 08:30:20 +0200

    Setting the body of the main function

--- Week 5 Lab/Lab Work/src/RollerCoaster.java
+++ Week 5 Lab/Lab Work/src/RollerCoaster.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class RollerCoaster {
     private String name;
     private String world;
@@ -16,4 +18,20 @@ public class RollerCoaster {
         return this.name + "(" + this.world + ")" + ": "+ this.time;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        RollerCoaster[] coasters = new RollerCoaster[n];
+        for (int i = 0; i<n ; i++){
+            String line = sc.nextLine();
+            String[] tokens = line.split(";");
+            RollerCoaster coaster = new RollerCoaster(
+                tokens[0],
+                tokens[1],
+                Integer.parseInt(tokens[2]),
+                Integer.parseInt(tokens[3])
+            );
+            coasters[i] = coaster;
+        }
+    }
 }
